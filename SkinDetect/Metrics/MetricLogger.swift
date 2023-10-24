041c6f5d8bb090ec45a333ea338f680934e6da29
Alin Petrus <alin.petrus@softvision.com>
2023-10-24 11:21:15 +0300

    updated plist

---
+++ SkinDetect/Metrics/MetricLogger.swift
@@ -0,0 +1,8 @@
+//
+//  MetricLogger.swift
+//  SkinDetect
+//
+//  Created by Alin Petrus on 19.10.2023.
+//
+
+import Foundation
---
+++ SkinDetect/Metrics/MetricsObserver.swift
@@ -0,0 +1,8 @@
+//
+//  MetricsObserver.swift
+//  SkinDetect
+//
+//  Created by Alin Petrus on 19.10.2023.
+//
+
+import Foundation
